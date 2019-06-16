3f1c1d9ff25a81aa621a0870785ff11eb41c5555
justyna <justyna.stanek.js@gmail.com>
2019-06-16 10:23:23 +0200

    add main

--- src/main/java/js/bmi/Main.java
+++ src/main/java/js/bmi/Main.java
@@ -1,8 +1,21 @@
 package js.bmi;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Provide you weight: ");
+        double w = sc.nextDouble();
+        System.out.println("Provide your height: ");
+        double h = sc.nextDouble();
+        sc.close();
+
+        BMI_Calculator bm1 = new BMI_Calculator(h,w);
+        System.out.println("Your BMI: " + bm1.calculate());
+        System.out.println("Interpretation: " + bm1.interpret());
+
     }
 }
