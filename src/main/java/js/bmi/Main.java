e632f4e73f104cc9b74a58f329758d3eb2bcb68b
justyna <justyna.stanek.js@gmail.com>
2019-06-16 11:00:06 +0200

    deal with exceptions

--- src/main/java/js/bmi/Main.java
+++ src/main/java/js/bmi/Main.java
@@ -1,21 +1,42 @@
 package js.bmi;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Provide you weight: ");
-        double w = sc.nextDouble();
-        System.out.println("Provide your height: ");
-        double h = sc.nextDouble();
-        sc.close();
+            Double w = null;
+            Double h = null;
 
-        BMI_Calculator bm1 = new BMI_Calculator(h,w);
-        System.out.println("Your BMI: " + bm1.calculate());
-        System.out.println("Interpretation: " + bm1.interpret());
+            Scanner sc = new Scanner(System.in);
+
+            while(w==null){
+                try {
+                    System.out.println("Provide your weight [in kg]: ");
+                    w = sc.nextDouble();
+                    sc.nextLine();
+                } catch(InputMismatchException ex){
+                    System.out.println("Provided value is incorrect.");
+                    sc.nextLine();
+                }
+            }
+            while(h==null){
+                try{
+                    System.out.println("Provide your height [in cm]: ");
+                    h = sc.nextDouble();
+                    sc.nextLine();
+                }catch(InputMismatchException ex){
+                    System.out.println("Provided value is incorrect.");
+                    sc.nextLine();}
+            }
+
+                sc.close();
+
+                BMI_Calculator bm1 = new BMI_Calculator(h, w);
+                System.out.println("Your BMI: " + bm1.calculate());
+                System.out.println("Interpretation: " + bm1.interpret());
 
     }
 }
